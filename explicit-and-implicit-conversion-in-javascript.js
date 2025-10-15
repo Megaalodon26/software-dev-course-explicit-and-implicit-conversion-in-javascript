caa1e35fb1254981d027624b70d1aa6419f62f34
Megaalodon26 <mmlynn175@gmail.com>
2025-10-14 23:28:04 -0400

    Submission changes for "Practice 1: Values, Data Types, and Operations - 3 - Debugging Type Conversion"

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,36 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
+/* Part 1: I added in the String() explicit type conversion method so that the output equals 3. 
+This fix works because I'm converting the 2 to a string to match the "5". */
 
-let result = "5" - 2;
+let result = String("5" - 2); 
 console.log("The result is: " + result);
+
+/* I left this one as is since it is already correct with the Boolean() explicit type converstion method */
 
 let isValid = Boolean("false");
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
+/* I added in the Number() explicit type converstion method so that the output equals 30 instead of 255.
+This fix works because it converts "25" as a string into an integer to be added to the integer 5. Since we're adding
+integers together and not concatenating strings. */
+
+let age = Number("25");
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+/* Part 2: */
+/* Implicit type converstion example: */
+let score = "10" + 8;
+console.log("The final score is: " + score);
+
+/* Explicit type converstion example: */
+let smallScore = null;
+let highScore = smallScore + 10;
+console.log("Here is your High Score: " + highScore);
+// convert null explicitly to a number (Number(null) === 0)
+let explicitHighScore = Number(smallScore) - 10;
+console.log("Here is your High Score: " + explicitHighScore);
